ae6917a44aa783b2e5f946526479483370252f99
damianof <damianof@users.noreply.github.com>
2025-06-28 10:26:02 -0400

    Fixed spelling of hover class

--- src/toast/AlertCssStrategy.ts
+++ src/toast/AlertCssStrategy.ts
@@ -5,17 +5,17 @@ const alertCssStrategy = new Map<string, string>([
 
 const iconCssStrategy = new Map<string, string>([
   ['normal', `content-[category] hover:bg-[category]`],
-  ['ghost', `text-[category] over:opacity-80`],
+  ['ghost', `text-[category] hover:opacity-80`],
 ])
 
 const textCssStrategy = new Map<string, string>([
   ['normal', `content-[category] hover:bg-[category]`],
-  ['ghost', `text-[category] over:opacity-80`],
+  ['ghost', `text-[category] hover:opacity-80`],
 ])
 
 const buttonCloseCssStrategy = new Map<string, string>([
   ['normal', `bg-[category] content-[category] hover:bg-[category] focus:ring-[category]-focus`],
-  ['ghost', `bg-transparent border-[category] text-[category] over:opacity-80 focus:ring-[category]-focus`],
+  ['ghost', `bg-transparent border-[category] text-[category] hover:opacity-80 focus:ring-[category]-focus`],
 ])
 
 export { alertCssStrategy, iconCssStrategy, textCssStrategy, buttonCloseCssStrategy }
